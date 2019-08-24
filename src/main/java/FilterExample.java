9f19df1eb0806bb0c92a780ef4b3359fce258280
Rajasekhar Karanam <karanam.rajasekhar44@gmail.com>
2019-08-25 00:31:16 +0530

    Project setup

--- src/main/java/FilterExample.java
+++ src/main/java/FilterExample.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
+import java.util.Arrays;
+import java.util.List;
 
 public class FilterExample {
+    public static void main(String[] args) {
+        List<String> names = Arrays.asList(
+                "James Gosling",
+                "Rod Johnson",
+                "Linus Torvalds",
+                "Gavin King",
+                "Brendan Eich"
+        );
+
+        // Java for-each
+        for (String founder : names) {
+            System.out.println(founder);
+        }
+
+        names.forEach(System.out::println);
+
+    }
 }
